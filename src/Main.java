03764ef9b39391ad1aa5e9011c9fec945378bcfd
Erodar <stijnrelou@gmail.com>
2023-05-22 10:31:52 +0200

    v4

--- src/Main.java
+++ src/Main.java
@@ -2,78 +2,59 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
+    public static int wincounter = 0;
     public static void main(String[] args) {
+        playgame();
+    }
+
+    public static void playgame() {
 
         Random rand = new Random();
-
         int RightNumber = rand.nextInt(1001);
-        int TurnCount = 1;
         int NumberGuess = 0;
 
-        boolean GuessingNow = true;
-        while (GuessingNow) {
+        for (int i =1; i < 10; i++) {
 
-            while (TurnCount < 11) {
-                Scanner NumGuess = new Scanner(System.in);
-                System.out.println("This is attempt " + TurnCount);
-                System.out.println("Guess the number: ");
-
+            Scanner NumGuess = new Scanner(System.in);
+            System.out.println("This is attempt " + i);
+            System.out.println("Guess the number: ");
 
             if (NumGuess.hasNextInt()) {
-                    NumberGuess = NumGuess.nextInt();
-                }
-
-            else if (!NumGuess.hasNextInt()) {
+                NumberGuess = NumGuess.nextInt();
+            } else if (!NumGuess.hasNextInt()) {
                 System.out.println("Please enter a valid number");
                 break;
             }
 
             if (NumberGuess == RightNumber) {
-                    System.out.println("You guessed the number!");
-                    TurnCount = 10;
+                System.out.println("You guessed the number!");
+                wincounter++;
+                break;
+            } else if (NumberGuess < RightNumber) {
+                System.out.println("Guess higher!");
+            } else {
+                System.out.println("Guess lower!");
             }
+        }
 
-            else if (NumberGuess < RightNumber) {
-                    System.out.println("Guess higher!");
-            }
+        System.out.println("The correct number was " + RightNumber);
+        Scanner PlayAgain = new Scanner(System.in);
+        System.out.println("Would you like to play again? y/n");
+        String YesSir = String.valueOf('y');
+        String NoSir = String.valueOf('n');
+        String PlayAgainChoice = PlayAgain.nextLine();
 
-            else {
-                    System.out.println("Guess lower!");
-            }
+        if (PlayAgainChoice.equals(YesSir)) {
+           playgame();
+        }
 
-            TurnCount += 1;
+        else if (PlayAgainChoice.equals(NoSir)) {
+            System.out.printf("You won %s times! \n", wincounter);
+            System.out.println("Thank-a you for-a playing my-a game!");
+        }
 
-            if (TurnCount == 11) {
-
-                    System.out.println("The correct number was " + RightNumber);
-                    while (TurnCount == 11) {
-
-                        Scanner PlayAgain = new Scanner(System.in);
-                        System.out.println("Would you like to play again? y/n");
-                        String YesSir = String.valueOf('y');
-                        String NoSir = String.valueOf('n');
-                        String PlayAgainChoice = PlayAgain.nextLine();
-
-                        if (PlayAgainChoice.equals(YesSir)) {
-                            RightNumber = rand.nextInt(1001);
-                            TurnCount = 1;
-                        }
-
-                        else if (PlayAgainChoice.equals(NoSir)) {
-
-                            GuessingNow = false;
-                            System.out.println("Thank-a you for-a playing my-a game!");
-                            break;
-
-                        }
-
-                        else {
-                            System.out.println("Please choose a valid option");
-                        }
-                    }
-                }
-            }
+        else {
+             System.out.println("Please choose a valid option");
         }
     }
 }
